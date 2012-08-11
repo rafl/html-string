cfb242ddcdac66bf0fd0fdd2aaec671af3b06061
Matt S Trout <mst@shadowcat.co.uk>
2012-08-11 17:32:14 +0000

    clean up TT override

--- lib/HTML/String/TT/Directive.pm
+++ lib/HTML/String/TT/Directive.pm
@@ -2,12 +2,13 @@ package HTML::String::TT::Directive;
 
 use strictures 1;
 use HTML::String::Overload ();
+use Data::Munge;
 use base qw(Template::Directive);
 
 sub template {
-    my $result = Template::Directive::pad(shift->SUPER::template(@_), 2);
-    $result =~ s/sub {/sub { use HTML::String::Overload;/;
-    $result;
+    return byval {
+        s/sub {/sub { use HTML::String::Overload;/;
+    } Template::Directive::pad(shift->SUPER::template(@_), 2);
 }
 
 1;
